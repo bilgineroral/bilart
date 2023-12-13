96bfdb2d2d5da46e92bf81ce26131e456b64c9df
bilgineroral <bilgineroral@gmail.com>
2023-12-13 18:39:54 +0300

    add cors settings

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from db.create_table import create_tables as util_create_tables
 from modules.modules import routers
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 for router in routers:
     app.include_router(router)
