21c30bcd9a722be25611c177e289b8ea02daeea5
Sardter <sadra701@gmail.com>
2023-12-13 16:18:02 +0200

    user trigger added

--- app/db/insert.py
+++ app/db/insert.py
@@ -5,10 +5,8 @@ from fastapi import HTTPException
 from db.model import Model
 from db.db import PgDatabase
 
-from db.retrieve import retrieve
-
 # insert data into any table
-def insert_data_into_table(table_name: str, data: dict, identifier: Any) -> Tuple[bool, str, Any]:
+def insert_data_into_table(table_name: str, data: dict, identifier: str) -> Tuple[bool, str, dict[str, Any]]:
     try:
         query = f"""INSERT INTO {table_name} ({', '.join(data.keys())}) 
             VALUES ({', '.join([f"'{v}'" for v in data.values()])}) RETURNING {identifier}"""
@@ -16,25 +14,23 @@ def insert_data_into_table(table_name: str, data: dict, identifier: Any) -> Tupl
         with PgDatabase() as db:
             # Execute the SQL query with the data values
             db.cursor.execute(query, list(data.values()))
-            
+            id = db.cursor.fetchone()[0]
             # Commit the transaction
             db.connection.commit()
             
-            id = db.cursor.fetchone()[0]
+            select_query = f"SELECT * FROM {table_name} WHERE {identifier} = {id}"
+            db.cursor.execute(select_query)
             
-            return True, "Data inserted successfully", id
+            row = db.cursor.fetchone()
+            columns: list[str] = [desc[0] for desc in db.cursor.description]
+            
+            return True, "Data inserted successfully", dict(zip(columns, row))
     except Exception as e:
-        print(e)
+        import traceback
+        traceback.print_exc()
+        
         raise HTTPException(status_code=500, detail=str(e))
 
 
 def insert(model: Model) -> Tuple[bool, str, dict[str, Any]]:
-    result, message, id = insert_data_into_table(model.get_table_name(), model.to_dict(), model.get_identifier())
-    
-    filters = {
-        'tables':[model],
-        'single':True,
-        f'{model.get_identifier()}':id
-    }
-    _, _, _, data = retrieve(**filters)
-    return result, message, data[0]
+    return insert_data_into_table(model.get_table_name(), model.to_dict(), model.get_identifier())
