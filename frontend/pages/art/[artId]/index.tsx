e85f22a02c3bb36e67974be0be1a28db35055ada
mrowaha <ashfaqrowaha@gmail.com>
2023-12-13 17:49:56 +0300

    art creation and view

--- frontend/pages/art/[artId]/index.tsx
+++ frontend/pages/art/[artId]/index.tsx
@@ -125,14 +125,14 @@ export default function ArtPage() {
               <DomainDivider color={theme.palette.primary.main} />
               <div style={{display :"flex", justifyContent: "space-between"}}>
                 <Typography variant="h4" color="#fff">
-                  TL {price}
+                  TL {artInfo?.price}
                 </Typography>
                 <Typography variant="h6" color="#fff">
                   starting price
                 </Typography> 
               </div>
               <Typography color="#fff">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                {artInfo?.description}
               </Typography>
               <ButtonGroup
                 variant="contained"
