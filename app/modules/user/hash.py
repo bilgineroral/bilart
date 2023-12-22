af7c3b92f04fe0ee7872ba35d8e5891ae87a1340
Sardter <sadra701@gmail.com>
2023-12-22 10:26:13 +0200

    aaaa

--- app/modules/user/hash.py
+++ app/modules/user/hash.py
@@ -1,7 +1,6 @@
-import bcrypt
-
-global salt
-salt = bcrypt.gensalt()
+import base64
 
 def hash_password(x: str) -> str:
-    return bcrypt.hashpw(x.encode('utf-8'), salt).decode('utf-8')
+    #return x
+    return base64.b64encode(x.encode('utf-8')).decode('utf-8')
+    # return bcrypt.hashpw(x.encode('utf-8'), salt).decode('utf-8')
